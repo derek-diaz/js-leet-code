9a01d94a44bc4749608ce48335d8049869b216f3
Derek Diaz Correa <gigabit77@gmail.com>
2022-07-06 23:03:45 -0500

    some more leetcode

--- 2-add-two-numbers.js
+++ 2-add-two-numbers.js
@@ -1,13 +1,5 @@
 const assert = require('assert');
 
-try {
-    assert.equal(sumDigits(1), 1);
-
-    console.log('PASSED: ' + 'sumDigits(1) should return 1');
-} catch (err) {
-    console.log(err);
-}
-
 function ListNode(val, next) {
     this.val = (val === undefined ? 0 : val)
     this.next = (next === undefined ? null : next)
@@ -28,9 +20,8 @@ var addTwoNumbersCarry = function (l1, l2, carry) {
         let val2 = l2 ? l2.val : 0;
         let sum = val1 + val2 + carry;
         let remainder = Math.trunc(sum/10);
-        if (l1 === null && l2 === null && sum > 0){
-            return new ListNode(Number(sum), addTwoNumbersCarry(l1, l2, remainder));
-        }else if (l1 === null){
+
+        if (l1 === null){
             return new ListNode(Number(String(sum).slice(-1)), addTwoNumbersCarry(l1, l2.next, remainder));
         }else if (l2 === null){
             return new ListNode(Number(String(sum).slice(-1)), addTwoNumbersCarry(l1.next, l2, remainder));
@@ -40,19 +31,4 @@ var addTwoNumbersCarry = function (l1, l2, carry) {
     }else if (carry > 0){
         return new ListNode(carry, null)
     }
-}
-
-
-try {
-    const l1t = new ListNode(3, new ListNode(4, new ListNode(2, null)))
-    const l2t = new ListNode(4, new ListNode(6, new ListNode(5, null)))
-
-    const expected = new ListNode(7, new ListNode(0, new ListNode(8, null)))
-
-    assert.equal(addTwoNumbers(l1t, l2t), expected);
-
-    console.log('PASSED: ' + 'addTwoNumbers() should return [7,0,8]');
-
-} catch (err) {
-    console.log(err);
 }
--- 3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters.js
@@ -8,10 +8,21 @@ var lengthOfLongestSubstring = function(s) {
     var longestSubstring = new Map();
     var currentStreak = new Map();
 
-    for (var )
-
-
-
+    for (var i = 0; i < s.length; i++) {
+        if (currentStreak.has(s.charAt(i))){
+          if (currentStreak.size > longestSubstring.size) {
+              longestSubstring = new Map(currentStreak);
+          }
+          currentStreak.clear();
+          currentStreak.set(s.charAt(i), s.charAt(i));
+        }else {
+            currentStreak.set(s.charAt(i), s.charAt(i));
+        }
+    }
+    if  ((currentStreak.size > 0 && longestSubstring.size === 0) || currentStreak.size > longestSubstring.size) {
+        return currentStreak.size;
+    }
+    return longestSubstring.size;
 };
 
 try {
@@ -35,6 +46,30 @@ try {
     assert.equal(lengthOfLongestSubstring("pwwkew"), 3);
 
     console.log('PASSED: ' + 'lengthOfLongestSubstring("pwwkew") should return 3');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(lengthOfLongestSubstring("pwwkew"), 3);
+
+    console.log('PASSED: ' + 'lengthOfLongestSubstring("pwwkew") should return 3');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(lengthOfLongestSubstring(" "), 1);
+
+    console.log('PASSED: ' + 'lengthOfLongestSubstring(" ") should return 1');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(lengthOfLongestSubstring("aab"), 2);
+
+    console.log('PASSED: ' + 'lengthOfLongestSubstring("aab") should return 2');
 } catch (err) {
     console.log(err);
 }
