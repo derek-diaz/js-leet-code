ba3ed8b64772b18862bc39c5e5651ee7d214e3dc
Derek Diaz Correa <gigabit77@gmail.com>
2022-07-09 21:53:22 -0500

    completed leetcode 58

--- 58-length-of-last-word.js
+++ 58-length-of-last-word.js
@@ -1,1 +1,33 @@
 const assert = require('assert');
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let arr = s.split(" ");
+    let filter = arr.filter(word => word !== "")
+    return filter[filter.length - 1].length;
+};
+
+
+try {
+    assert.equal(lengthOfLastWord("Hello World"), 5);
+    console.log('PASSED: ' + ' lengthOfLastWord should return 5');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(lengthOfLastWord("   fly me   to   the moon  "), 4);
+    console.log('PASSED: ' + ' lengthOfLastWord should return 4');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(lengthOfLastWord("luffy is still joyboy"), 6);
+    console.log('PASSED: ' + ' lengthOfLastWord should return 6');
+} catch (err) {
+    console.log(err);
+}
